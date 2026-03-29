ef1607e2da95c2c586ca2c01149bb07104c83401
samer <judosamer555@gmail.com>
2026-03-29 20:50:06 +0200

    update the package

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      exports: "default",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
+  ],
+  external: ["react", "react-dom"],
+  plugins: [
+    babel({
+      babelHelpers: "bundled",
+      presets: [
+        ["@babel/preset-env", { targets: "> 0.25%, not dead" }],
+        ["@babel/preset-react", { runtime: "classic" }],
+      ],
+      extensions: [".js", ".jsx"],
+      exclude: "node_modules/**",
+    }),
+    resolve({ extensions: [".js", ".jsx"] }),
+    commonjs(),
+    terser(),
+  ],
+};
